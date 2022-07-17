d6891206634c2e3d017741149749c34644833c17
Ismail <ismajovic20@gmail.com>
2022-07-17 15:00:33 +0200

    init

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,17 +1,23 @@
 import './ExpenceItem.css';
 
-function ExpenceItem (){
+function ExpenceItem (props){
+    /*const expenceDate = new Date()
+    const expenceTitle = 'Mobile Phone'
+    const expenceAmount = 247.654; Mozemo ih koristiti lookalno ocv epodatke*/
+
+
     return (
       <div className='expence-item'>
-        <div>16 jul 2022</div>
+        <div>{props.date.toISOString()}</div>     
         <div className='expence-item__description'>
-          <h2>Phone mobile!</h2>
-          <div className='expence-item__price'>$247.654</div>
+          <h2>{props.title}</h2>
+          <div className='expence-item__price'>${props.amount}</div>
         </div>
       </div>  
     )
 }  
-    
+    //toString i toDateString daju razlicite podateke o vremenu, toString daje opsirinije podatke
+    //toISOString() inspect this function
 ///return <h2>Expence Item!</h2>
 // if you want can used a arrow function
 
